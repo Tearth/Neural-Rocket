0baa988156613a31b3c7c23b2eec930fe3f6ff4e
tearth <tearthdev@gmail.com>
2020-02-04 14:52:37 +0100

    Add ground and sky

---
+++ Neural Rocket/Assets/Scripts/CameraEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEntity : MonoBehaviour
+{
+    [Header("General")]
+    public Camera Camera;
+
+    [Header("Sky")]
+    public Color BaseSkyColor;
+    public float BlackSkyHeight;
+
+    void Update()
+    {
+        UpdateSkyColor();
+    }
+
+    private void UpdateSkyColor()
+    {
+        var fixedHeight = Mathf.Clamp(transform.position.y, 0, BlackSkyHeight);
+        var multiplier = Math.Abs(fixedHeight) < float.Epsilon ? 1 : 1 - fixedHeight / BlackSkyHeight;
+
+        Camera.backgroundColor = BaseSkyColor * multiplier;
+    }
+}
---
+++ Neural Rocket/Assets/Scripts/GroundEntity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GroundEntity : MonoBehaviour
+{
+    [Header("General")]
+    public Camera MainCamera;
+
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        transform.position = new Vector3(
+            MainCamera.transform.position.x,
+            transform.position.y,
+            MainCamera.transform.position.z
+        );
+    }
+}
