5e3439b59e337956015204b034ca49404d4ea06b
tearth <tearthdev@gmail.com>
2020-02-06 20:47:14 +0100

    Add ability to spawn multiple launchpads and rockets

--- Neural Rocket/Assets/Scripts/AgentsAcademy.cs
+++ Neural Rocket/Assets/Scripts/AgentsAcademy.cs
@@ -1,14 +1,21 @@
 ﻿using MLAgents;
+using UnityEngine;
 
 public class AgentsAcademy : Academy
 {
-    public override void AcademyReset()
+    [Header("General")]
+    public GameObject RocketTarget;
+    public GameObject Launchpad;
+    public int AgentsCount;
+    public float SpaceBetweenAgents;
+    public int AgentsInRow;
+
+    private void Start()
     {
-
-    }
-
-    public override void AcademyStep()
-    {
-
+        for (var i = 1; i < AgentsCount; i++)
+        {
+            Instantiate(RocketTarget, RocketTarget.transform.position + new Vector3((i % AgentsInRow) * SpaceBetweenAgents, 0, (i / AgentsInRow) * SpaceBetweenAgents), Quaternion.identity, transform);
+            Instantiate(Launchpad, Launchpad.transform.position + new Vector3((i % AgentsInRow) * SpaceBetweenAgents, 0, (i / AgentsInRow) * SpaceBetweenAgents), Quaternion.identity, transform);
+        }
     }
 }
